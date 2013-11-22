0255d9889024e809772c2e930e007ad5ea0a5a3d
Ján Mochňak <janmochnak@gmail.com>
2013-11-22 13:16:04 +0100

    Replaced old .block with .panel-block in HTML module template.

--- html/mod_custom/default.php
+++ html/mod_custom/default.php
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 ?>
 
-<div class="block custom<?php echo $moduleclass_sfx ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage');?>)"<?php endif;?> >
+<div class="panel-block custom<?php echo $moduleclass_sfx ?>" <?php if ($params->get('backgroundimage')) : ?> style="background-image:url(<?php echo $params->get('backgroundimage');?>)"<?php endif;?> >
   <h2>
     <?php echo $module->title;?>
   </h2>
