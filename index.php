ed4ff63422f630f5eef5a5118918590d581f90a0
Ján Mochňak <janmochnak@gmail.com>
2013-11-22 13:07:09 +0100

    Remove right panel content and place position-7 template tags instead. Removed style key in position-7 tag.

--- index.php
+++ index.php
@@ -34,36 +34,13 @@ $doc->addStyleSheet('templates/' . $this->template . '/css/default.css');
 		<jdoc:include type="modules" name="position-3" />
 		<jdoc:include type="message" />
 		<jdoc:include type="component" />
-		<jdoc:include type="modules" name="position-2" style="none" />
+		<jdoc:include type="modules" name="position-2" />
 	</section>
 
 	<aside id="panel-right">
 		<div class="panel">
-			<jdoc:include type="modules" name="position-7" style="well" />
+			<jdoc:include type="modules" name="position-7" />
 		</div>
-<!--		<section>
-			<h3>Bleskovky</h3>
-			<article class="flash">
-				<header>Kritériá prijímacieho konania</header>
-				<p>Zverejnili sme kritériá prijímacieho konania pre školský rok 2013/2014.</p>
-				<p><a href="#">Výsledková listina</a></p>
-			</article>
-			<article class="flash">
-				<header>Dievčatá a chlapci 9. ročníkov ZŠ</header>
-				<p>Naša škola pre Vás pred prijímacími skúškami pripravila 5 grátis stretnutí Odpoludnie s matematikou..</p>
-			</article>
-			<article class="flash">
-				<header>NAG2013</header>
-				<p>Prinášame výsledky.</p>
-			</article>
-		</section>
-		<section>
-			<h3>Rýchla navigácia</h3>
-		</section>
-		<section>
-			<h3>Anketa</h3>
-		</section> -->
-
 	</aside>
 
 	<footer>
